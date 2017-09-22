363969372a2b07522ce12c9a852896ecb52654a7
xu.mingxun <xu.mingxun@puscene.com>
2017-09-22 11:59:45 +0800

    add

--- app_keras.py
+++ app_keras.py
@@ -59,7 +59,6 @@ def build3(_name):
     model.add(Conv2D(filters=48, kernel_size=3, strides=1, padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=3, strides=2, padding='same'))
 
-
     model.add(Flatten())
     model.add(Dropout(0.3))
 
@@ -81,7 +80,6 @@ def build4(_name):
     model.add(Conv2D(filters=48, kernel_size=4, strides=1, padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=4, strides=2, padding='same'))
 
-
     model.add(Flatten())
     model.add(Dropout(0.3))
 
@@ -102,7 +100,6 @@ def build5(_name):
     model.add(Conv2D(filters=24, kernel_size=5, strides=1, padding='same', activation='relu'))
     model.add(Conv2D(filters=48, kernel_size=5, strides=1, padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=5, strides=2, padding='same'))
-
 
     model.add(Flatten())
     model.add(Dropout(0.3))
@@ -237,10 +234,33 @@ def build11(_name):
     model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
+def build12(_name):
+    model = Sequential(name=_name)
+
+    model.add(Conv2D(input_shape=(40, 40, 3), filters=6, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(Conv2D(filters=12, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(Conv2D(filters=24, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=3, strides=2, padding='same'))
+
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(filters=48, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(Conv2D(filters=96, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(Conv2D(filters=192, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=3, strides=2, padding='same'))
+
+    model.add(Flatten())
+    model.add(Dropout(0.3))
+
+    model.add(Dense(units=62, activation='softmax'))
+
+    model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
 # model=build1("x1")
 # model=build2("x2")
 # model=build3("x3") #收敛快
-model=build4("x4")
+# model=build4("x4")
 # model=build5("x5")
 # model=build6("x6")
 # model=build7("x7")
@@ -248,6 +268,8 @@ model=build4("x4")
 # model=build9("x9")
 # model=build10("x10")
 # model=build11("x11")
+
+model=build12("x12")
 
 print(model.summary())
 
